383374aa4bdee2542e76a745cf7e65920fe74596
superman <1501814877@qq.com>
2019-10-10 17:10:55 +0800

    将react_navigation_demo移除

--- demo/react_navigation_demo/App.js
+++
@@ -1,4 +0,0 @@
-import {AppStackNavigator,MaterialTopTabNavigator,BottomTabNavigator} from './navigators/AppNavigators'
-import SwitchNavigator from './navigators/SwitchNavigator'
-import {createAppContainer} from 'react-navigation'
-export default createAppContainer(AppStackNavigator);
