b410036b3f881548f39e0906d2694ba2e1427ce8
superman <1501814877@qq.com>
2019-10-28 10:53:37 +0800

    GitHubTrending库升级到3.1.3

--- js/model/TimeSpan.js
+++ js/model/TimeSpan.js
@@ -4,14 +4,14 @@
  * @param searchText
  * @constructor
  */
-export default function TimeSpan(showText, searchText) {
-    this.showText = showText;
-    this.searchText = searchText;
-}
-
-// export default class TimeSpan {
-//     TimeSpan(showText, searchText) {
-//         this.showText = showText;
-//         this.searchText = searchText;
-//     }
+// export default function TimeSpan(showText, searchText) {
+//     this.showText = showText;
+//     this.searchText = searchText;
 // }
+
+export default class TimeSpan {
+    constructor(showText, searchText) {
+        this.showText = showText;
+        this.searchText = searchText;
+    }
+}
